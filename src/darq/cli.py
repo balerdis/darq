balerdis/pegasus-harness

fix(cli): el CLI ya no arrastra curses cuando nadie abre la TUI
pegasus.cli importaba pegasus.tui.app a nivel de módulo, y ese módulo hace
import curses también a nivel de módulo. En un Python compilado sin la
extensión _curses (caso real reportado), esa cadena corría al cargar el
CLI y tiraba abajo --version, doctor --json e install por igual, aunque
ninguno abre una pantalla.

El import de pegasus.tui.app ahora es perezoso, adentro del único lugar
que lo usa (la rama que abre el menú al no darle ningún comando en una
terminal). Y si aun así falla por falta de _curses, ese punto de entrada
ya no deja pasar el traceback: informa en una frase que falta _curses,
qué suele significar y qué hacer, y el resto de los comandos ni se
enteran.

src/pegasus/cli.py
@@ -63,7 +63,6 @@ from pegasus.ports.mcp_process import MCPProcess
 from pegasus.ports.model_assignment_store import ModelAssignmentStoreError
 from pegasus.ports.npm_installer import NpmInstaller
 from pegasus.ports.snapshot_store import SnapshotStoreError
-from pegasus.tui import app as tui_app
 
 NODE_BINARY = "node"
 
@@ -184,6 +183,19 @@ def main(argv: list[str] | None = None, *, runtime: Runtime | None = None) -> in
         # read it, so the usage line is still the honest answer -- and it is
         # what a script that called this by mistake needs to see.
         if _attached_to_a_terminal():
+            # Imported here, not at module level: `pegasus.tui.app` imports
+            # `curses`, which does not exist on every Python (see its own
+            # entry point's docstring) -- every other command on this CLI
+            # must keep working on such a build, and would not if importing
+            # this module ran the moment `pegasus.cli` itself was loaded.
+            try:
+                from pegasus.tui import app as tui_app
+            except ModuleNotFoundError as error:
+                if error.name != "_curses":
+                    raise
+                runtime.out.write(_missing_curses_message(runtime.identity) + "\n")
+                return FAILED
+
             tui_app.main()
             return OK
         parser.print_usage(runtime.out)
@@ -3585,6 +3597,26 @@ def _attached_to_a_terminal() -> bool:
         return sys.stdin.isatty() and sys.stdout.isatty()
     except ValueError:
         return False
+
+
+def _missing_curses_message(identity: Identity) -> str:
+    """What to say instead of a `ModuleNotFoundError` traceback when the menu's
+    own `curses` import fails because `_curses` was never built.
+
+    A real report: a hand-built Python 3.12 compiled without the ncurses
+    development headers has no `_curses` extension module at all, which is
+    exactly the shape `ModuleNotFoundError` this catches names. Every other
+    command still runs without it -- only the menu needs a real terminal
+    screen -- so this names the cause, what it means, and the way out,
+    instead of a traceback that looks like the whole program is broken.
+    """
+    return (
+        f"{identity.program_name}'s menu needs curses, and this python has no _curses "
+        "extension module -- usually a python built without the ncurses development "
+        "headers present at build time. every other command still works; rebuild "
+        f"python with ncurses support to use the menu, or run `{identity.program_name} "
+        "<command>` directly instead"
+    )
 
 
 COMMANDS = {

tests/test_cli.py
@@ -28,6 +28,7 @@ import io
 import json
 import os
 import stat
+import sys
 import tempfile
 import tomllib
 import unittest
@@ -2475,6 +2476,87 @@ class _Stream:
 
     def isatty(self) -> bool:
         return self._terminal
+
+
+class _BlockCurses:
+    """A `sys.meta_path` finder that makes `import curses` fail exactly the
+    way it does on a Python built without the `_curses` extension: a real
+    `ModuleNotFoundError` naming `_curses`, not a stand-in exception -- see
+    `tests/test_cli_without_curses.py` for the full-process version of the
+    same fault, reproduced there via subprocess instead of module patching."""
+
+    def find_spec(self, name, path=None, target=None):
+        if name == "curses" or name.startswith("curses."):
+            raise ModuleNotFoundError("No module named '_curses'", name="_curses")
+        return None
+
+
+class MissingCursesTest(unittest.TestCase):
+    """The one place `main()` opens the TUI: what it does when `curses` --
+    imported lazily, on purpose, so every other command stays unaffected --
+    turns out not to exist on this interpreter."""
+
+    def _forget_tui_app(self):
+        """`pegasus.tui.app` (and `curses` itself) must be re-imported for the
+        blocked finder to have anything to intercept; a module already
+        cached in `sys.modules` from an earlier test would make this test
+        vacuous."""
+        removed = {
+            name: module
+            for name, module in sys.modules.items()
+            if name == "curses" or name.startswith("curses.")
+            or name == "pegasus.tui.app" or name.startswith("pegasus.tui.app.")
+        }
+        for name in removed:
+            del sys.modules[name]
+        self.addCleanup(sys.modules.update, removed)
+
+    def test_reports_an_honest_message_instead_of_a_traceback(self):
+        self._forget_tui_app()
+        finder = _BlockCurses()
+        cli.sys.meta_path.insert(0, finder)
+        self.addCleanup(cli.sys.meta_path.remove, finder)
+
+        out = io.StringIO()
+        runtime = replace(cli.default_runtime(out), out=out)
+        with mock.patch.object(cli, "_attached_to_a_terminal", return_value=True):
+            code = cli.main([], runtime=runtime)
+
+        self.assertEqual(code, cli.FAILED)
+        message = out.getvalue()
+        self.assertIn("_curses", message)
+        self.assertIn(runtime.identity.program_name, message)
+        self.assertNotIn("Traceback", message)
+
+    def test_an_unrelated_missing_module_is_not_swallowed(self):
+        """Only `_curses` gets the honest rewrite -- any other
+        `ModuleNotFoundError` raised while importing the TUI is a real bug
+        and must still surface as itself."""
+        self._forget_tui_app()
+
+        class _BlockSomethingElse:
+            def find_spec(self, name, path=None, target=None):
+                if name == "pegasus.tui.app":
+                    raise ModuleNotFoundError("No module named 'nonexistent_thing'", name="nonexistent_thing")
+                return None
+
+        finder = _BlockSomethingElse()
+        cli.sys.meta_path.insert(0, finder)
+        self.addCleanup(cli.sys.meta_path.remove, finder)
+
+        runtime = cli.default_runtime(io.StringIO())
+        with mock.patch.object(cli, "_attached_to_a_terminal", return_value=True):
+            with self.assertRaises(ModuleNotFoundError) as raised:
+                cli.main([], runtime=runtime)
+        self.assertEqual(raised.exception.name, "nonexistent_thing")
+
+    def test_missing_curses_message_names_cause_meaning_and_remedy(self):
+        identity = cli.default_identity()
+        message = cli._missing_curses_message(identity)
+        self.assertIn("_curses", message)
+        self.assertIn("ncurses", message)
+        self.assertIn(identity.program_name, message)
+        self.assertTrue(message[0].islower())
 
 
 class FailurePrefixCompositionTest(unittest.TestCase):

tests/test_cli_without_curses.py
@@ -1,0 +1,103 @@
+"""Tests that pegasus's non-interactive surface works when `_curses` is unavailable.
+
+This reproduces a real user report: a hand-built Python 3.12, compiled in `/usr/local`
+without the ncurses headers, has no `_curses` extension module at all. Before the fix,
+`pegasus.cli` imported `pegasus.tui.app` at module load time (`from pegasus.tui import
+app as tui_app`), and `pegasus.tui.app` does `import curses` at its own module level --
+so the import chain ran on every invocation of the CLI, whether or not the TUI was ever
+opened. `--version`, `doctor --json` and `install` all failed with the same
+`ModuleNotFoundError: No module named '_curses'`, even though none of them touch a
+terminal screen.
+
+`_curses` is blocked with a `sys.meta_path` hook installed in a subprocess before
+`pegasus` is ever imported -- the same shape `tests/test_version_guard.py` uses to
+fabricate an old interpreter before `pegasus.__main__` runs, so the hook is in place
+for the *first* import of anything under `pegasus`, not retrofitted after the fact.
+"""
+from __future__ import annotations
+
+import subprocess
+import sys
+import tempfile
+import unittest
+from pathlib import Path
+
+ROOT = Path(__file__).resolve().parents[1]
+SRC = ROOT / "src"
+
+#: Installed before anything under `pegasus` is imported, so the very first `import
+#: curses` (transitively or directly) anywhere in the process sees `_curses` as absent
+#: -- exactly what the reported interpreter actually has.
+_BLOCK_CURSES_PRELUDE = (
+    "import sys, importlib.abc\n"
+    "class _BlockCurses(importlib.abc.MetaPathFinder):\n"
+    "    def find_spec(self, name, path=None, target=None):\n"
+    "        if name == '_curses' or name.startswith('_curses.'):\n"
+    "            raise ModuleNotFoundError(\"No module named '_curses'\")\n"
+    "        return None\n"
+    "sys.meta_path.insert(0, _BlockCurses())\n"
+    "sys.path.insert(0, %r)\n"
+) % str(SRC)
+
+
+def _run_blocked(argv: list[str], home: Path, *, path: str = "/usr/bin:/bin") -> subprocess.CompletedProcess:
+    script = _BLOCK_CURSES_PRELUDE + (
+        "import runpy\n"
+        "sys.argv = ['pegasus'] + %r\n"
+        "runpy.run_module('pegasus.__main__', run_name='__main__')\n"
+    ) % list(argv)
+    env = {
+        "PATH": path,
+        "HOME": str(home),
+        "XDG_DATA_HOME": str(home / "data"),
+    }
+    return subprocess.run(
+        [sys.executable, "-c", script],
+        cwd=str(ROOT), env=env, capture_output=True, text=True, timeout=30,
+    )
+
+
+def _stub_opencode(stub_dir: Path) -> None:
+    """A fake `opencode` on `PATH`, just present enough for `install` to find
+    it -- `install` refuses to write configuration for a CLI it cannot see
+    on this machine, which has nothing to do with the defect under test
+    here."""
+    stub_dir.mkdir(parents=True, exist_ok=True)
+    opencode = stub_dir / "opencode"
+    opencode.write_text("#!/bin/sh\necho opencode 1.0.0\n", encoding="utf-8")
+    opencode.chmod(0o755)
+
+
+class NonInteractiveSurfaceWithoutCursesTest(unittest.TestCase):
+    """Every command reachable without opening the TUI must not need `_curses`."""
+
+    def test_version_works_without_curses(self):
+        with tempfile.TemporaryDirectory() as home:
+            result = _run_blocked(["--version"], Path(home))
+        self.assertEqual(result.returncode, 0, result.stderr)
+        self.assertNotIn("_curses", result.stderr)
+        self.assertNotIn("Traceback", result.stderr)
+
+    def test_doctor_json_works_without_curses(self):
+        with tempfile.TemporaryDirectory() as home:
+            result = _run_blocked(["doctor", "--json"], Path(home))
+        self.assertEqual(result.returncode, 0, result.stderr)
+        self.assertNotIn("_curses", result.stderr)
+        self.assertNotIn("Traceback", result.stderr)
+
+    def test_install_works_without_curses(self):
+        with tempfile.TemporaryDirectory() as home:
+            stub_dir = Path(home) / "stub-bin"
+            _stub_opencode(stub_dir)
+            result = _run_blocked(
+                ["install", "--cli", "opencode", "--mcp", "none"],
+                Path(home),
+                path=f"{stub_dir}:/usr/bin:/bin",
+            )
+        self.assertEqual(result.returncode, 0, result.stdout + result.stderr)
+        self.assertNotIn("_curses", result.stderr)
+        self.assertNotIn("Traceback", result.stderr)
+
+
+if __name__ == "__main__":
+    unittest.main()

balerdis/darq

src/darq/cli.py
@@ -63,7 +63,6 @@ from darq.ports.mcp_process import MCPProcess
 from darq.ports.model_assignment_store import ModelAssignmentStoreError
 from darq.ports.npm_installer import NpmInstaller
 from darq.ports.snapshot_store import SnapshotStoreError
-from darq.tui import app as tui_app
 
 NODE_BINARY = "node"
 
@@ -184,6 +183,19 @@ def main(argv: list[str] | None = None, *, runtime: Runtime | None = None) -> in
         # read it, so the usage line is still the honest answer -- and it is
         # what a script that called this by mistake needs to see.
         if _attached_to_a_terminal():
+            # Imported here, not at module level: `darq.tui.app` imports
+            # `curses`, which does not exist on every Python (see its own
+            # entry point's docstring) -- every other command on this CLI
+            # must keep working on such a build, and would not if importing
+            # this module ran the moment `darq.cli` itself was loaded.
+            try:
+                from darq.tui import app as tui_app
+            except ModuleNotFoundError as error:
+                if error.name != "_curses":
+                    raise
+                runtime.out.write(_missing_curses_message(runtime.identity) + "\n")
+                return FAILED
+
             tui_app.main()
             return OK
         parser.print_usage(runtime.out)
@@ -3585,6 +3597,26 @@ def _attached_to_a_terminal() -> bool:
         return sys.stdin.isatty() and sys.stdout.isatty()
     except ValueError:
         return False
+
+
+def _missing_curses_message(identity: Identity) -> str:
+    """What to say instead of a `ModuleNotFoundError` traceback when the menu's
+    own `curses` import fails because `_curses` was never built.
+
+    A real report: a hand-built Python 3.12 compiled without the ncurses
+    development headers has no `_curses` extension module at all, which is
+    exactly the shape `ModuleNotFoundError` this catches names. Every other
+    command still runs without it -- only the menu needs a real terminal
+    screen -- so this names the cause, what it means, and the way out,
+    instead of a traceback that looks like the whole program is broken.
+    """
+    return (
+        f"{identity.program_name}'s menu needs curses, and this python has no _curses "
+        "extension module -- usually a python built without the ncurses development "
+        "headers present at build time. every other command still works; rebuild "
+        f"python with ncurses support to use the menu, or run `{identity.program_name} "
+        "<command>` directly instead"
+    )
 
 
 COMMANDS = {

tests/test_cli.py
@@ -28,6 +28,7 @@ import io
 import json
 import os
 import stat
+import sys
 import tempfile
 import tomllib
 import unittest
@@ -2484,6 +2485,87 @@ class _Stream:
 
     def isatty(self) -> bool:
         return self._terminal
+
+
+class _BlockCurses:
+    """A `sys.meta_path` finder that makes `import curses` fail exactly the
+    way it does on a Python built without the `_curses` extension: a real
+    `ModuleNotFoundError` naming `_curses`, not a stand-in exception -- see
+    `tests/test_cli_without_curses.py` for the full-process version of the
+    same fault, reproduced there via subprocess instead of module patching."""
+
+    def find_spec(self, name, path=None, target=None):
+        if name == "curses" or name.startswith("curses."):
+            raise ModuleNotFoundError("No module named '_curses'", name="_curses")
+        return None
+
+
+class MissingCursesTest(unittest.TestCase):
+    """The one place `main()` opens the TUI: what it does when `curses` --
+    imported lazily, on purpose, so every other command stays unaffected --
+    turns out not to exist on this interpreter."""
+
+    def _forget_tui_app(self):
+        """`darq.tui.app` (and `curses` itself) must be re-imported for the
+        blocked finder to have anything to intercept; a module already
+        cached in `sys.modules` from an earlier test would make this test
+        vacuous."""
+        removed = {
+            name: module
+            for name, module in sys.modules.items()
+            if name == "curses" or name.startswith("curses.")
+            or name == "darq.tui.app" or name.startswith("darq.tui.app.")
+        }
+        for name in removed:
+            del sys.modules[name]
+        self.addCleanup(sys.modules.update, removed)
+
+    def test_reports_an_honest_message_instead_of_a_traceback(self):
+        self._forget_tui_app()
+        finder = _BlockCurses()
+        cli.sys.meta_path.insert(0, finder)
+        self.addCleanup(cli.sys.meta_path.remove, finder)
+
+        out = io.StringIO()
+        runtime = replace(cli.default_runtime(out), out=out)
+        with mock.patch.object(cli, "_attached_to_a_terminal", return_value=True):
+            code = cli.main([], runtime=runtime)
+
+        self.assertEqual(code, cli.FAILED)
+        message = out.getvalue()
+        self.assertIn("_curses", message)
+        self.assertIn(runtime.identity.program_name, message)
+        self.assertNotIn("Traceback", message)
+
+    def test_an_unrelated_missing_module_is_not_swallowed(self):
+        """Only `_curses` gets the honest rewrite -- any other
+        `ModuleNotFoundError` raised while importing the TUI is a real bug
+        and must still surface as itself."""
+        self._forget_tui_app()
+
+        class _BlockSomethingElse:
+            def find_spec(self, name, path=None, target=None):
+                if name == "darq.tui.app":
+                    raise ModuleNotFoundError("No module named 'nonexistent_thing'", name="nonexistent_thing")
+                return None
+
+        finder = _BlockSomethingElse()
+        cli.sys.meta_path.insert(0, finder)
+        self.addCleanup(cli.sys.meta_path.remove, finder)
+
+        runtime = cli.default_runtime(io.StringIO())
+        with mock.patch.object(cli, "_attached_to_a_terminal", return_value=True):
+            with self.assertRaises(ModuleNotFoundError) as raised:
+                cli.main([], runtime=runtime)
+        self.assertEqual(raised.exception.name, "nonexistent_thing")
+
+    def test_missing_curses_message_names_cause_meaning_and_remedy(self):
+        identity = cli.default_identity()
+        message = cli._missing_curses_message(identity)
+        self.assertIn("_curses", message)
+        self.assertIn("ncurses", message)
+        self.assertIn(identity.program_name, message)
+        self.assertTrue(message[0].islower())
 
 
 class FailurePrefixCompositionTest(unittest.TestCase):

tests/test_cli_without_curses.py
@@ -1,0 +1,103 @@
+"""Tests that pegasus's non-interactive surface works when `_curses` is unavailable.
+
+This reproduces a real user report: a hand-built Python 3.12, compiled in `/usr/local`
+without the ncurses headers, has no `_curses` extension module at all. Before the fix,
+`darq.cli` imported `darq.tui.app` at module load time (`from darq.tui import
+app as tui_app`), and `darq.tui.app` does `import curses` at its own module level --
+so the import chain ran on every invocation of the CLI, whether or not the TUI was ever
+opened. `--version`, `doctor --json` and `install` all failed with the same
+`ModuleNotFoundError: No module named '_curses'`, even though none of them touch a
+terminal screen.
+
+`_curses` is blocked with a `sys.meta_path` hook installed in a subprocess before
+`pegasus` is ever imported -- the same shape `tests/test_version_guard.py` uses to
+fabricate an old interpreter before `darq.__main__` runs, so the hook is in place
+for the *first* import of anything under `pegasus`, not retrofitted after the fact.
+"""
+from __future__ import annotations
+
+import subprocess
+import sys
+import tempfile
+import unittest
+from pathlib import Path
+
+ROOT = Path(__file__).resolve().parents[1]
+SRC = ROOT / "src"
+
+#: Installed before anything under `pegasus` is imported, so the very first `import
+#: curses` (transitively or directly) anywhere in the process sees `_curses` as absent
+#: -- exactly what the reported interpreter actually has.
+_BLOCK_CURSES_PRELUDE = (
+    "import sys, importlib.abc\n"
+    "class _BlockCurses(importlib.abc.MetaPathFinder):\n"
+    "    def find_spec(self, name, path=None, target=None):\n"
+    "        if name == '_curses' or name.startswith('_curses.'):\n"
+    "            raise ModuleNotFoundError(\"No module named '_curses'\")\n"
+    "        return None\n"
+    "sys.meta_path.insert(0, _BlockCurses())\n"
+    "sys.path.insert(0, %r)\n"
+) % str(SRC)
+
+
+def _run_blocked(argv: list[str], home: Path, *, path: str = "/usr/bin:/bin") -> subprocess.CompletedProcess:
+    script = _BLOCK_CURSES_PRELUDE + (
+        "import runpy\n"
+        "sys.argv = ['pegasus'] + %r\n"
+        "runpy.run_module('darq.__main__', run_name='__main__')\n"
+    ) % list(argv)
+    env = {
+        "PATH": path,
+        "HOME": str(home),
+        "XDG_DATA_HOME": str(home / "data"),
+    }
+    return subprocess.run(
+        [sys.executable, "-c", script],
+        cwd=str(ROOT), env=env, capture_output=True, text=True, timeout=30,
+    )
+
+
+def _stub_opencode(stub_dir: Path) -> None:
+    """A fake `opencode` on `PATH`, just present enough for `install` to find
+    it -- `install` refuses to write configuration for a CLI it cannot see
+    on this machine, which has nothing to do with the defect under test
+    here."""
+    stub_dir.mkdir(parents=True, exist_ok=True)
+    opencode = stub_dir / "opencode"
+    opencode.write_text("#!/bin/sh\necho opencode 1.0.0\n", encoding="utf-8")
+    opencode.chmod(0o755)
+
+
+class NonInteractiveSurfaceWithoutCursesTest(unittest.TestCase):
+    """Every command reachable without opening the TUI must not need `_curses`."""
+
+    def test_version_works_without_curses(self):
+        with tempfile.TemporaryDirectory() as home:
+            result = _run_blocked(["--version"], Path(home))
+        self.assertEqual(result.returncode, 0, result.stderr)
+        self.assertNotIn("_curses", result.stderr)
+        self.assertNotIn("Traceback", result.stderr)
+
+    def test_doctor_json_works_without_curses(self):
+        with tempfile.TemporaryDirectory() as home:
+            result = _run_blocked(["doctor", "--json"], Path(home))
+        self.assertEqual(result.returncode, 0, result.stderr)
+        self.assertNotIn("_curses", result.stderr)
+        self.assertNotIn("Traceback", result.stderr)
+
+    def test_install_works_without_curses(self):
+        with tempfile.TemporaryDirectory() as home:
+            stub_dir = Path(home) / "stub-bin"
+            _stub_opencode(stub_dir)
+            result = _run_blocked(
+                ["install", "--cli", "opencode", "--mcp", "none"],
+                Path(home),
+                path=f"{stub_dir}:/usr/bin:/bin",
+            )
+        self.assertEqual(result.returncode, 0, result.stdout + result.stderr)
+        self.assertNotIn("_curses", result.stderr)
+        self.assertNotIn("Traceback", result.stderr)
+
+
+if __name__ == "__main__":
+    unittest.main()