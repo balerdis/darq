balerdis/pegasus-harness

feat(roles): el readiness fuera de SDD lo declara quien pidió el trabajo
El orquestador ya no dice que ningún "listo" es posible hasta que hable
sdd-verify. Esa espera queda para SDD, en su propia oración. En L0 y FTD,
el readiness lo declara quien pidió el trabajo, a partir de lo observado,
y si lo escribió él mismo lo dice.

king-pegasus dice lo mismo junto a "Close the loop you open", y en FTD
lleva su propio record, al que llega por la escalera sin nombrar el
procedimiento. pegasus-implementer y pegasus-general dicen que el record
lo lleva quien coordina y que ellos le devuelven evidencia.

pegasus-verifier no cambia: sigue devolviendo solo evidencia, y un agente
que escribe nunca puede llegar a quien firma. ReadinessAuthorityScopeTest
pasa sin haberlo tocado, porque ninguna frase nueva reclama autoridad.

Los tests son de mundo cerrado. En cada archivo, las frases que hablan de
readiness o del record tienen que ser exactamente las fijadas. La lista de
palabras de readiness suma "shippable", "LGTM", "mergeable", "wrap up" y
"close out", porque la revisión mostró que "shippable" se escapaba.

tests/test_orchestrator_routing.py
@@ -120,6 +120,69 @@ FLOW_APPLICABILITY_WORD_CEILING = 1061
 #: own session, with that pointer's fail-open. 715 + 43 + 25 = 783.
 KING_PEGASUS_WORD_CEILING = 783
 
+#: Readiness, by stem -- a claim that something is ready, done or signed off,
+#: in any wording: "ready", "readiness", "declares", "sign-off", "approved",
+#: "verdict", "done", "finished", "complete", "accepted", "good to go",
+#: "green light". A stem list is finite, and that is its declared residue:
+#: "ship" is left out because `pegasus-general` uses it for something else
+#: ("CLIs ship their own built-in sub-agent types").
+READINESS_STEM = re.compile(
+    r"\bready\b|readiness|declar|\bsign(?:s|ed|ing)?\b|sign-?off|approv|verdict|\bdone\b|finish"
+    r"|\bcomplete(?:d|s)?\b|\bcompletion\b|accept|good to go|green light"
+    # Review found "shippable" missing; the rest of this line is the same
+    # register a person reaches for when calling work finished.
+    r"|shippable|ship it|\blgtm\b|merge-?able|merge-ready|wrap(?:s|ped|ping)? (?:it )?up|close(?:s|d)? (?:it )?out",
+    re.IGNORECASE,
+)
+#: The FTD record, by stem, including the words it could be called instead.
+RECORD_STEM = re.compile(r"record|\blog\b|checklist|docs/ftd", re.IGNORECASE)
+
+#: The permitted sentences of slice (c), pinned as written. Closed world: the
+#: pinned sentence must be there exactly once, and no other sentence of the
+#: same file may touch the same subject -- an inverted, passive, doubly negated
+#: or reworded sentence fails, and a legitimate rewording is a deliberate edit
+#: of the pin.
+WARMTH_SDD = (
+    "In SDD, until `sdd-verify` has spoken, the friendliest honest sentence available to you is the one "
+    "naming what is still missing."
+)
+READINESS_OUTSIDE_SDD = (
+    "In L0 and FTD, readiness is yours to declare as whoever asked for the work, from observed evidence; "
+    "if you wrote it yourself, say so."
+)
+#: Every sentence of the orchestrator on the readiness subject, as written.
+#: The first four predate slice (c); `ReadinessAuthorityScopeTest` still owns
+#: the claim they carry, untouched.
+ORCHESTRATOR_READINESS_SENTENCES = frozenset(
+    {
+        "For every executable or configuration change delivered through SDD, delegate a fresh `sdd-verify` "
+        "before declaring the change ready to archive.",
+        "`sdd-verify` is Pegasus's sole authority for declaring that an SDD change is ready to archive.",
+        "A sub-agent's claim is evidence, not a verdict.",
+        "Read it with the same suspicion you would read a claim from anyone else: a phase that reports done "
+        "while showing no runtime proof has reported an intention, and saying so is your job rather than an "
+        "accusation.",
+        "Warmth is never a readiness claim.",
+        READINESS_OUTSIDE_SDD,
+    }
+)
+
+
+def sentences_of(text: str) -> list[str]:
+    """Every sentence of `text`, whitespace collapsed, a leading list marker
+    stripped."""
+    found = []
+    for part in re.split(r"(?<=[.!?])\s+", " ".join(text.split())):
+        part = re.sub(r"^(?:- |\d+\. )", "", part).strip()
+        if part:
+            found.append(part)
+    return found
+
+
+def sentences_on(text: str, stem: re.Pattern[str]) -> set[str]:
+    return {sentence for sentence in sentences_of(text) if stem.search(sentence)}
+
+
 #: The bodies a word ceiling guards, each with the ceiling it answers to.
 CEILINGED_BODIES = (
     (ORCHESTRATOR, ORCHESTRATOR_WORD_CEILING),
@@ -298,6 +361,35 @@ class FlowApplicabilityTest(unittest.TestCase):
         for name in ("pegasus-explorer", "pegasus-verifier", "pegasus-implementer"):
             with self.subTest(agent=name):
                 self.assertIn(name, prose)
+
+
+class ReadinessOutsideSddTest(unittest.TestCase):
+    """Slice (c), option (a): outside SDD, readiness is declared by whoever
+    asked, from observed evidence, and signing what you wrote is said as such;
+    "until `sdd-verify` has spoken" is scoped to SDD in its own sentence.
+
+    Closed world, not a negation classifier: the permitted sentences are pinned
+    as written, and every sentence of the orchestrator that touches readiness
+    or the FTD record must be one of them."""
+
+    def setUp(self):
+        self.text = ORCHESTRATOR.read_text(encoding="utf-8")
+        self.flat = " ".join(self.text.split())
+
+    def test_readiness_outside_sdd_is_declared_by_whoever_asked(self):
+        self.assertEqual(self.flat.count(READINESS_OUTSIDE_SDD), 1)
+
+    def test_waiting_for_sdd_verify_is_scoped_to_sdd_in_its_own_sentence(self):
+        self.assertEqual(self.flat.count(WARMTH_SDD), 1)
+        self.assertEqual(self.flat.count("until `sdd-verify` has spoken"), 1)
+
+    def test_no_other_sentence_speaks_of_readiness(self):
+        self.assertEqual(sentences_on(self.text, READINESS_STEM), set(ORCHESTRATOR_READINESS_SENTENCES))
+
+    def test_the_orchestrator_says_nothing_about_who_keeps_the_record(self):
+        """That rule lives in the procedure and in the specialists; a sentence
+        here would be a second, unpinned owner."""
+        self.assertEqual(sentences_on(self.text, RECORD_STEM), set())
 
 
 class PreflightStaysStrictTest(unittest.TestCase):

tests/test_persona_split.py
@@ -29,7 +29,16 @@ from pathlib import Path
 
 from pegasus.core import content as content_module
 from test_flow_routing import declared_facts, flow_text
-from test_orchestrator_routing import APPLICABILITY_NEEDLE
+from test_orchestrator_routing import APPLICABILITY_NEEDLE, READINESS_STEM, RECORD_STEM, sentences_on
+
+#: Slice (c): the teaching voice's readiness and record sentences, pinned as
+#: written. Closed world: each is there exactly once, in its rule, and no other
+#: sentence of the persona touches readiness or the record.
+KING_READINESS = (
+    "Readiness is yours to declare, only from what you observed; since you wrote the change, say so instead "
+    "of presenting it as independent verification."
+)
+KING_RECORD = "On FTD you keep the record yourself; that file leads you to how it is kept."
 
 ROOT = Path(__file__).resolve().parents[1] / "src" / "pegasus" / "content"
 BASELINE = ROOT / "system-prompt" / "AGENTS.md"
@@ -289,6 +298,30 @@ class PersonaTest(SharedContentRules, unittest.TestCase):
         # `test_content.test_the_teaching_voice_declares_every_server_this_release_ships`
         # asserts by deriving the set from the content tree. A second hardcoded
         # copy would be one more place to go stale when a server is added.
+
+    def rule(self, needle: str) -> str:
+        rules = self.text.split("\n## Rules\n", 1)[1].split("\n## ", 1)[0]
+        bullets = [" ".join(line[2:].split()) for line in rules.split("\n") if line.startswith("- ")]
+        found = [bullet for bullet in bullets if needle in bullet]
+        self.assertEqual(len(found), 1, f"one rule must carry {needle!r}")
+        return found[0]
+
+    def test_readiness_sits_beside_close_the_loop(self):
+        self.assertIn(KING_READINESS, self.rule("Close the loop you open"))
+        self.assertEqual(" ".join(self.text.split()).count(KING_READINESS), 1)
+
+    def test_it_keeps_the_ftd_record_itself_through_the_ladder(self):
+        """In the rule that names the ladder, the only file the voice reaches
+        the procedure through: the procedure itself is never named here."""
+        self.assertIn(KING_RECORD, self.rule("{{skills_root}}/_shared/flow-applicability.md"))
+        self.assertEqual(" ".join(self.text.split()).count(KING_RECORD), 1)
+        self.assertNotIn("ftd-procedure.md", self.text)
+
+    def test_no_other_sentence_speaks_of_readiness(self):
+        self.assertEqual(sentences_on(self.text, READINESS_STEM), {KING_READINESS})
+
+    def test_no_other_sentence_speaks_of_the_record(self):
+        self.assertEqual(sentences_on(self.text, RECORD_STEM), {KING_RECORD})
 
     def test_the_persona_carries_the_voice_sections(self):
         """A set, not a sequence: `## Persona Scope` promises these exist."""

tests/test_phase_less_specialists.py
@@ -36,6 +36,35 @@ from pegasus.adapters.opencode import render as render_module
 from pegasus.core import content as content_module
 from pegasus.core.content import AgentMode
 from pegasus.core.types import ConfigKeyArtifact, Environment
+from test_orchestrator_routing import READINESS_STEM, RECORD_STEM, sentences_on
+
+#: Slice (c): inside an FTD, whoever coordinates keeps the record and a
+#: specialist returns evidence. Pinned as written, in `## Result identity`.
+SPECIALIST_RECORD = "Inside an FTD, whoever coordinates it keeps the record; you return evidence for it."
+#: The writers this applies to. `pegasus-verifier` is left out on purpose: it
+#: never writes and does not change in slice (c).
+RECORD_RETURNERS = ("pegasus-implementer", "pegasus-general")
+#: Every sentence of each writer on the readiness subject, as written, all of
+#: them from before slice (c). Closed world: nothing may join them unpinned.
+SPECIALIST_READINESS_SENTENCES = {
+    "pegasus-implementer": frozenset(
+        {
+            "Read before you write, and prove the change works before you call it done.",
+            "The evidence you get back is evidence, never a sign-off: no agent you can reach has the standing to "
+            "call a change ready, and neither do you.",
+            "Not a narration of the diff, and not a claim about the change being ready.",
+        }
+    ),
+    "pegasus-general": frozenset(
+        {
+            "This is deliberate — the ten SDD phase agents each own a phase and return a phase-shaped result "
+            "(`sdd-explore` an `## Exploration: {topic}`, `sdd-verify` a `## Verification Report`), so a fan-out "
+            "of phase-agent copies would produce several competing phase reports, `sdd-verify` copies included, "
+            "each declaring the same change ready to archive.",
+        }
+    ),
+}
+
 
 ROOT = Path(__file__).resolve().parents[1]
 CONTENT = ROOT / "src" / "pegasus" / "content"
@@ -502,6 +531,23 @@ class WhatEachOneReturnsTest(unittest.TestCase):
     def test_the_explorer_returns_a_finding_and_the_implementer_what_changed(self):
         self.assertIn("finding", body("pegasus-explorer").lower())
         self.assertIn("what changed", body("pegasus-implementer").lower())
+
+    def test_a_writer_leaves_the_record_to_whoever_coordinates_and_returns_evidence(self):
+        for name in RECORD_RETURNERS:
+            with self.subTest(agent=name):
+                identity = " ".join(body(name).split("## Result identity", 1)[1].split())
+                self.assertIn(SPECIALIST_RECORD, identity)
+                self.assertEqual(" ".join(whole(name).split()).count(SPECIALIST_RECORD), 1)
+
+    def test_no_other_sentence_of_a_writer_speaks_of_the_record(self):
+        for name in RECORD_RETURNERS:
+            with self.subTest(agent=name):
+                self.assertEqual(sentences_on(whole(name), RECORD_STEM), {SPECIALIST_RECORD})
+
+    def test_no_new_readiness_sentence_joins_a_writer(self):
+        for name in RECORD_RETURNERS:
+            with self.subTest(agent=name):
+                self.assertEqual(sentences_on(whole(name), READINESS_STEM), set(SPECIALIST_READINESS_SENTENCES[name]))
 
     def test_each_contract_is_advertised_where_a_caller_reads_it_too(self):
         """The one place a contract legitimately lives twice.

balerdis/darq

tests/test_orchestrator_routing.py
@@ -125,6 +125,69 @@ FLOW_APPLICABILITY_WORD_CEILING = 1061
 #: own session, with that pointer's fail-open. 715 + 43 + 25 = 783.
 ARQUITECTO_DARQ_WORD_CEILING = 783
 
+#: Readiness, by stem -- a claim that something is ready, done or signed off,
+#: in any wording: "ready", "readiness", "declares", "sign-off", "approved",
+#: "verdict", "done", "finished", "complete", "accepted", "good to go",
+#: "green light". A stem list is finite, and that is its declared residue:
+#: "ship" is left out because `darq-general` uses it for something else
+#: ("CLIs ship their own built-in sub-agent types").
+READINESS_STEM = re.compile(
+    r"\bready\b|readiness|declar|\bsign(?:s|ed|ing)?\b|sign-?off|approv|verdict|\bdone\b|finish"
+    r"|\bcomplete(?:d|s)?\b|\bcompletion\b|accept|good to go|green light"
+    # Review found "shippable" missing; the rest of this line is the same
+    # register a person reaches for when calling work finished.
+    r"|shippable|ship it|\blgtm\b|merge-?able|merge-ready|wrap(?:s|ped|ping)? (?:it )?up|close(?:s|d)? (?:it )?out",
+    re.IGNORECASE,
+)
+#: The FTD record, by stem, including the words it could be called instead.
+RECORD_STEM = re.compile(r"record|\blog\b|checklist|docs/ftd", re.IGNORECASE)
+
+#: The permitted sentences of slice (c), pinned as written. Closed world: the
+#: pinned sentence must be there exactly once, and no other sentence of the
+#: same file may touch the same subject -- an inverted, passive, doubly negated
+#: or reworded sentence fails, and a legitimate rewording is a deliberate edit
+#: of the pin.
+WARMTH_SDD = (
+    "In SDD, until `sdd-verify` has spoken, the friendliest honest sentence available to you is the one "
+    "naming what is still missing."
+)
+READINESS_OUTSIDE_SDD = (
+    "In L0 and FTD, readiness is yours to declare as whoever asked for the work, from observed evidence; "
+    "if you wrote it yourself, say so."
+)
+#: Every sentence of the orchestrator on the readiness subject, as written.
+#: The first four predate slice (c); `ReadinessAuthorityScopeTest` still owns
+#: the claim they carry, untouched.
+ORCHESTRATOR_READINESS_SENTENCES = frozenset(
+    {
+        "For every executable or configuration change delivered through SDD, delegate a fresh `sdd-verify` "
+        "before declaring the change ready to archive.",
+        "`sdd-verify` is DARQ's sole authority for declaring that an SDD change is ready to archive.",
+        "A sub-agent's claim is evidence, not a verdict.",
+        "Read it with the same suspicion you would read a claim from anyone else: a phase that reports done "
+        "while showing no runtime proof has reported an intention, and saying so is your job rather than an "
+        "accusation.",
+        "Warmth is never a readiness claim.",
+        READINESS_OUTSIDE_SDD,
+    }
+)
+
+
+def sentences_of(text: str) -> list[str]:
+    """Every sentence of `text`, whitespace collapsed, a leading list marker
+    stripped."""
+    found = []
+    for part in re.split(r"(?<=[.!?])\s+", " ".join(text.split())):
+        part = re.sub(r"^(?:- |\d+\. )", "", part).strip()
+        if part:
+            found.append(part)
+    return found
+
+
+def sentences_on(text: str, stem: re.Pattern[str]) -> set[str]:
+    return {sentence for sentence in sentences_of(text) if stem.search(sentence)}
+
+
 #: The bodies a word ceiling guards, each with the ceiling it answers to.
 CEILINGED_BODIES = (
     (ORCHESTRATOR, ORCHESTRATOR_WORD_CEILING),
@@ -303,6 +366,35 @@ class FlowApplicabilityTest(unittest.TestCase):
         for name in ("darq-explorer", "darq-verifier", "darq-implementer"):
             with self.subTest(agent=name):
                 self.assertIn(name, prose)
+
+
+class ReadinessOutsideSddTest(unittest.TestCase):
+    """Slice (c), option (a): outside SDD, readiness is declared by whoever
+    asked, from observed evidence, and signing what you wrote is said as such;
+    "until `sdd-verify` has spoken" is scoped to SDD in its own sentence.
+
+    Closed world, not a negation classifier: the permitted sentences are pinned
+    as written, and every sentence of the orchestrator that touches readiness
+    or the FTD record must be one of them."""
+
+    def setUp(self):
+        self.text = ORCHESTRATOR.read_text(encoding="utf-8")
+        self.flat = " ".join(self.text.split())
+
+    def test_readiness_outside_sdd_is_declared_by_whoever_asked(self):
+        self.assertEqual(self.flat.count(READINESS_OUTSIDE_SDD), 1)
+
+    def test_waiting_for_sdd_verify_is_scoped_to_sdd_in_its_own_sentence(self):
+        self.assertEqual(self.flat.count(WARMTH_SDD), 1)
+        self.assertEqual(self.flat.count("until `sdd-verify` has spoken"), 1)
+
+    def test_no_other_sentence_speaks_of_readiness(self):
+        self.assertEqual(sentences_on(self.text, READINESS_STEM), set(ORCHESTRATOR_READINESS_SENTENCES))
+
+    def test_the_orchestrator_says_nothing_about_who_keeps_the_record(self):
+        """That rule lives in the procedure and in the specialists; a sentence
+        here would be a second, unpinned owner."""
+        self.assertEqual(sentences_on(self.text, RECORD_STEM), set())
 
 
 class PreflightStaysStrictTest(unittest.TestCase):

tests/test_persona_split.py
@@ -29,7 +29,16 @@ from pathlib import Path
 
 from darq.core import content as content_module
 from test_flow_routing import declared_facts, flow_text
-from test_orchestrator_routing import APPLICABILITY_NEEDLE
+from test_orchestrator_routing import APPLICABILITY_NEEDLE, READINESS_STEM, RECORD_STEM, sentences_on
+
+#: Slice (c): the teaching voice's readiness and record sentences, pinned as
+#: written. Closed world: each is there exactly once, in its rule, and no other
+#: sentence of the persona touches readiness or the record.
+ARQUITECTO_DARQ_READINESS = (
+    "Readiness is yours to declare, only from what you observed; since you wrote the change, say so instead "
+    "of presenting it as independent verification."
+)
+ARQUITECTO_DARQ_RECORD = "On FTD you keep the record yourself; that file leads you to how it is kept."
 
 ROOT = Path(__file__).resolve().parents[1] / "src" / "darq" / "content"
 BASELINE = ROOT / "system-prompt" / "AGENTS.md"
@@ -289,6 +298,30 @@ class PersonaTest(SharedContentRules, unittest.TestCase):
         # `test_content.test_the_teaching_voice_declares_every_server_this_release_ships`
         # asserts by deriving the set from the content tree. A second hardcoded
         # copy would be one more place to go stale when a server is added.
+
+    def rule(self, needle: str) -> str:
+        rules = self.text.split("\n## Rules\n", 1)[1].split("\n## ", 1)[0]
+        bullets = [" ".join(line[2:].split()) for line in rules.split("\n") if line.startswith("- ")]
+        found = [bullet for bullet in bullets if needle in bullet]
+        self.assertEqual(len(found), 1, f"one rule must carry {needle!r}")
+        return found[0]
+
+    def test_readiness_sits_beside_close_the_loop(self):
+        self.assertIn(ARQUITECTO_DARQ_READINESS, self.rule("Close the loop you open"))
+        self.assertEqual(" ".join(self.text.split()).count(ARQUITECTO_DARQ_READINESS), 1)
+
+    def test_it_keeps_the_ftd_record_itself_through_the_ladder(self):
+        """In the rule that names the ladder, the only file the voice reaches
+        the procedure through: the procedure itself is never named here."""
+        self.assertIn(ARQUITECTO_DARQ_RECORD, self.rule("{{skills_root}}/_shared/flow-applicability.md"))
+        self.assertEqual(" ".join(self.text.split()).count(ARQUITECTO_DARQ_RECORD), 1)
+        self.assertNotIn("ftd-procedure.md", self.text)
+
+    def test_no_other_sentence_speaks_of_readiness(self):
+        self.assertEqual(sentences_on(self.text, READINESS_STEM), {ARQUITECTO_DARQ_READINESS})
+
+    def test_no_other_sentence_speaks_of_the_record(self):
+        self.assertEqual(sentences_on(self.text, RECORD_STEM), {ARQUITECTO_DARQ_RECORD})
 
     def test_the_persona_carries_the_voice_sections(self):
         """A set, not a sequence: `## Persona Scope` promises these exist."""

tests/test_phase_less_specialists.py
@@ -36,6 +36,35 @@ from darq.adapters.opencode import render as render_module
 from darq.core import content as content_module
 from darq.core.content import AgentMode
 from darq.core.types import ConfigKeyArtifact, Environment
+from test_orchestrator_routing import READINESS_STEM, RECORD_STEM, sentences_on
+
+#: Slice (c): inside an FTD, whoever coordinates keeps the record and a
+#: specialist returns evidence. Pinned as written, in `## Result identity`.
+SPECIALIST_RECORD = "Inside an FTD, whoever coordinates it keeps the record; you return evidence for it."
+#: The writers this applies to. `darq-verifier` is left out on purpose: it
+#: never writes and does not change in slice (c).
+RECORD_RETURNERS = ("darq-implementer", "darq-general")
+#: Every sentence of each writer on the readiness subject, as written, all of
+#: them from before slice (c). Closed world: nothing may join them unpinned.
+SPECIALIST_READINESS_SENTENCES = {
+    "darq-implementer": frozenset(
+        {
+            "Read before you write, and prove the change works before you call it done.",
+            "The evidence you get back is evidence, never a sign-off: no agent you can reach has the standing to "
+            "call a change ready, and neither do you.",
+            "Not a narration of the diff, and not a claim about the change being ready.",
+        }
+    ),
+    "darq-general": frozenset(
+        {
+            "This is deliberate — the ten SDD phase agents each own a phase and return a phase-shaped result "
+            "(`sdd-explore` an `## Exploration: {topic}`, `sdd-verify` a `## Verification Report`), so a fan-out "
+            "of phase-agent copies would produce several competing phase reports, `sdd-verify` copies included, "
+            "each declaring the same change ready to archive.",
+        }
+    ),
+}
+
 
 ROOT = Path(__file__).resolve().parents[1]
 CONTENT = ROOT / "src" / "darq" / "content"
@@ -502,6 +531,23 @@ class WhatEachOneReturnsTest(unittest.TestCase):
     def test_the_explorer_returns_a_finding_and_the_implementer_what_changed(self):
         self.assertIn("finding", body("darq-explorer").lower())
         self.assertIn("what changed", body("darq-implementer").lower())
+
+    def test_a_writer_leaves_the_record_to_whoever_coordinates_and_returns_evidence(self):
+        for name in RECORD_RETURNERS:
+            with self.subTest(agent=name):
+                identity = " ".join(body(name).split("## Result identity", 1)[1].split())
+                self.assertIn(SPECIALIST_RECORD, identity)
+                self.assertEqual(" ".join(whole(name).split()).count(SPECIALIST_RECORD), 1)
+
+    def test_no_other_sentence_of_a_writer_speaks_of_the_record(self):
+        for name in RECORD_RETURNERS:
+            with self.subTest(agent=name):
+                self.assertEqual(sentences_on(whole(name), RECORD_STEM), {SPECIALIST_RECORD})
+
+    def test_no_new_readiness_sentence_joins_a_writer(self):
+        for name in RECORD_RETURNERS:
+            with self.subTest(agent=name):
+                self.assertEqual(sentences_on(whole(name), READINESS_STEM), set(SPECIALIST_READINESS_SENTENCES[name]))
 
     def test_each_contract_is_advertised_where_a_caller_reads_it_too(self):
         """The one place a contract legitimately lives twice.