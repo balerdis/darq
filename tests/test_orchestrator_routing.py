balerdis/pegasus-harness

feat(routing): king-pegasus apunta a la escalera, y la escalera y king tienen techo
king-pegasus cubre todas las rutas salvo SDD. Cuando la ruta no es obvia
lee flow-applicability.md, con el mismo fail-open que el resto de sus
punteros. El trabajo que resulta ser SDD lo nombra y se lo pasa al
orquestador; nunca lo corre él. Es un puntero y no una copia de la
escalera: un test prohíbe que king nombre los routing facts o el texto de
la escalera. Su may_delegate_to no cambia.

El test de la derivación exige que la viñeta diga que pasa el trabajo al
orquestador, que la negación caiga sobre "run yourself" y que nada niegue
el pase. Antes alcanzaba con que "never" y "run" aparecieran en la misma
frase, y una viñeta que decía lo contrario pasaba igual.

flow-applicability.md y king-pegasus.md ganan techo de palabras propio:
1061 y 783. Son lo medido más lo reservado para los agregados que el plan
ya nombra, cada reserva estimada con un borrador de su frase y sin holgura
redonda. Los tres techos cuentan el archivo entero, front matter incluido,
y lo dicen en su comentario. El test que impide esquivar un techo
reacomodando líneas ahora recorre los tres cuerpos.

tests/test_orchestrator_routing.py
@@ -39,6 +39,7 @@ SHARED = SKILLS / "_shared"
 
 ORCHESTRATOR = AGENTS / "pegasus-orchestrator.md"
 APPLICABILITY = SHARED / "flow-applicability.md"
+KING_PEGASUS = AGENTS / "king-pegasus.md"
 CRITERION = SHARED / "sub-delegation-criterion.md"
 
 #: Gate 1's own wording. The orchestrator must point at it, never restate it.
@@ -81,12 +82,48 @@ FTD_NOT_SDD_STEMS = ("trade-off", "session", "record", "outliv", "surviv")
 #: `WordCeilingIsReformatProofTest` below, which proves this on a rewrapped
 #: copy rather than asserting it).
 #:
-#: 1327 words today. The ceiling carries the same proportional headroom the
-#: retired line ceiling did (104 / 97 ~= 1.072x), rounded down: 1327 * 1.07 ~=
-#: 1420. That is room for a short new sentence, not a second section -- a
-#: change that needs more than that earns a deliberate ceiling bump, not a
-#: reflow.
+#: 1327 words when the ceiling was set; 1398 after v7 rewrote the routing
+#: section text for text, which leaves the margin slice (c) needs. The
+#: ceiling carries the same proportional headroom the retired line ceiling
+#: did (104 / 97 ~= 1.072x), rounded down: 1327 * 1.07 ~= 1420. That is
+#: room for a short new sentence, not a second section -- a change that
+#: needs more than that earns a deliberate ceiling bump, not a reflow.
 ORCHESTRATOR_WORD_CEILING = 1420
+
+#: What every ceiling below measures: the WHOLE file, as `wc -w` and
+#: `len(text.split())` count it -- front matter included -- exactly as
+#: `ORCHESTRATOR_WORD_CEILING` above always has, even where prose calls the
+#: file "the body". Subtracting the front matter re-derives a number these
+#: tests never check. Each ceiling is the count measured when it was set plus
+#: only the words already reserved for additions the v7 plan names: no
+#: round-number slack, so drift beyond those additions is caught.
+#:
+#: `flow-applicability.md`: 1026 words measured after v7 a3 (it has no front
+#: matter, so here whole file and prose agree). Reserved: 35 words for b5's
+#: pointer to `_shared/ftd-procedure.md` at the exit of the FTD route clause --
+#: one sentence naming the procedure (record, evidence, close), that it is read
+#: only on that route, and its fail-open, estimated from a draft of that
+#: sentence. 1026 + 35 = 1061. Anything past that pointer earns a deliberate
+#: bump, never a reflow.
+FLOW_APPLICABILITY_WORD_CEILING = 1061
+
+#: `king-pegasus.md`: 715 words measured after v7 a5 -- 33 of them front
+#: matter, its two `---` delimiters included. Reserved, estimated from a draft
+#: of each sentence: 43 words for c3 -- about 30 for the readiness wording
+#: beside "Close the loop you open" (outside SDD the readiness call is this
+#: voice's own, made from what it observed, and signing what it wrote is said
+#: as such) and about 13 for the rule that on FTD it keeps the record itself,
+#: reached through `flow-applicability.md`; and 25 words for d5 -- naming
+#: `_shared/implementation-craft.md` and resolving Strict TDD Mode once in its
+#: own session, with that pointer's fail-open. 715 + 43 + 25 = 783.
+KING_PEGASUS_WORD_CEILING = 783
+
+#: The bodies a word ceiling guards, each with the ceiling it answers to.
+CEILINGED_BODIES = (
+    (ORCHESTRATOR, ORCHESTRATOR_WORD_CEILING),
+    (APPLICABILITY, FLOW_APPLICABILITY_WORD_CEILING),
+    (KING_PEGASUS, KING_PEGASUS_WORD_CEILING),
+)
 
 
 def rewrap_preserving_words(text: str) -> str:
@@ -320,34 +357,53 @@ class OrchestratorStaysSmallTest(unittest.TestCase):
         self.assertLessEqual(words, ORCHESTRATOR_WORD_CEILING, "the orchestrator body grew")
 
 
+class FlowApplicabilityStaysSmallTest(unittest.TestCase):
+    """The ladder is read whenever a route is not obvious; it must not grow into
+    the manual of every flow. How FTD runs belongs to its own procedure."""
+
+    def test_the_ladder_did_not_grow_in_hiding(self):
+        words = len(APPLICABILITY.read_text(encoding="utf-8").split())
+        self.assertLessEqual(words, FLOW_APPLICABILITY_WORD_CEILING, "flow-applicability.md grew")
+
+
+class KingPegasusStaysSmallTest(unittest.TestCase):
+    """An always-on body: every word is paid on every turn of the voice."""
+
+    def test_the_teaching_voice_did_not_grow(self):
+        words = len(KING_PEGASUS.read_text(encoding="utf-8").split())
+        self.assertLessEqual(words, KING_PEGASUS_WORD_CEILING, "king-pegasus.md grew")
+
+
 class WordCeilingIsReformatProofTest(unittest.TestCase):
     """The decisive guard: the measure must not move when only line breaks do.
 
-    Rewraps the real orchestrator body in memory -- the repository file is
-    read and never written -- and asserts the word count this suite relies on is
+    Rewraps each ceilinged body in memory -- the repository files are read and
+    never written -- and asserts the word count this suite relies on is
     unchanged while the line count is, proving the incident in the comment
     above cannot recur under the new measure the way it did under the old one.
     """
 
     def test_rewrapping_moves_lines_but_not_words(self):
-        original = ORCHESTRATOR.read_text(encoding="utf-8")
-        rewrapped = rewrap_preserving_words(original)
+        for body, _ in CEILINGED_BODIES:
+            with self.subTest(body=body.name):
+                original = body.read_text(encoding="utf-8")
+                rewrapped = rewrap_preserving_words(original)
 
-        original_words = len(original.split())
-        rewrapped_words = len(rewrapped.split())
-        original_lines = len(original.splitlines())
-        rewrapped_lines = len(rewrapped.splitlines())
+                original_words = len(original.split())
+                rewrapped_words = len(rewrapped.split())
+                original_lines = len(original.splitlines())
+                rewrapped_lines = len(rewrapped.splitlines())
 
-        self.assertEqual(
-            original_words,
-            rewrapped_words,
-            "the rewrap changed the word count -- it is not a pure reflow",
-        )
-        self.assertNotEqual(
-            original_lines,
-            rewrapped_lines,
-            "the rewrap did not actually change the line layout -- the test proves nothing",
-        )
+                self.assertEqual(
+                    original_words,
+                    rewrapped_words,
+                    "the rewrap changed the word count -- it is not a pure reflow",
+                )
+                self.assertNotEqual(
+                    original_lines,
+                    rewrapped_lines,
+                    "the rewrap did not actually change the line layout -- the test proves nothing",
+                )
 
 
 if __name__ == "__main__":

tests/test_persona_split.py
@@ -28,6 +28,8 @@ import unittest
 from pathlib import Path
 
 from pegasus.core import content as content_module
+from test_flow_routing import declared_facts, flow_text
+from test_orchestrator_routing import APPLICABILITY_NEEDLE
 
 ROOT = Path(__file__).resolve().parents[1] / "src" / "pegasus" / "content"
 BASELINE = ROOT / "system-prompt" / "AGENTS.md"
@@ -297,6 +299,67 @@ class PersonaTest(SharedContentRules, unittest.TestCase):
             if not declares(found, level, prefix)
         ]
         self.assertEqual(missing, [], f"the persona lost sections; it has {found}")
+
+
+class KingPegasusRoutesTest(unittest.TestCase):
+    """The teaching voice takes every route but SDD, and reaches the ladder lazily.
+
+    `_shared/flow-applicability.md` owns the four routes; this voice only needs
+    to know where that file is, what to do when it cannot be read, and that
+    work which turns out to need SDD is named and handed to the orchestrator
+    rather than run here. Anything more would be the ladder restated in an
+    always-on body. Like the rest of this module it pins structure -- a path,
+    the house fail-open phrase, two names in one rule -- never a sentence.
+    """
+
+    POINTER = "{{skills_root}}/_shared/flow-applicability.md"
+
+    @classmethod
+    def setUpClass(cls):
+        cls.text = PERSONA.read_text(encoding="utf-8")
+        rules = cls.text.split("\n## Rules\n", 1)[1].split("\n## ", 1)[0]
+        cls.bullets = [" ".join(line[2:].split()) for line in rules.split("\n") if line.startswith("- ")]
+        cls.agent = next(a for a in content_module.load().agents if a.name == "king-pegasus")
+
+    def bullets_naming(self, *needles: str) -> list[str]:
+        return [bullet for bullet in self.bullets if all(needle in bullet for needle in needles)]
+
+    def test_a_rule_points_at_the_route_ladder(self):
+        self.assertTrue(self.bullets_naming(self.POINTER), self.bullets)
+
+    def test_the_pointer_fails_open(self):
+        """In the same rule, so the fallback belongs to this reference and not to
+        the delegation-capabilities one that already says it."""
+        self.assertTrue(self.bullets_naming(self.POINTER, "missing or unreadable"), self.bullets)
+
+    def test_work_that_needs_sdd_is_handed_to_the_orchestrator_not_run_here(self):
+        rules = self.bullets_naming("SDD", "`pegasus-orchestrator`")
+        self.assertTrue(rules, self.bullets)
+        # The negation has to govern "run ... yourself" and the hand-over has to
+        # stand un-negated: a bullet that merely has "never" somewhere before
+        # "run" also passes when it says the opposite ("never hand it over --
+        # run it yourself"), which is the one reading this rule forbids.
+        handed_over = re.compile(r"\bhand(?:s|ed)?\s+(?:it\s+)?over\s+to\s+`pegasus-orchestrator`")
+        never_run_here = re.compile(r"\b(?:never|not)\s+run\s+(?:it\s+)?yourself\b")
+        negated_hand_over = re.compile(r"\b(?:never|not|no)\b[^.]*\bhand")
+        self.assertTrue(
+            any(
+                handed_over.search(rule) and never_run_here.search(rule) and not negated_hand_over.search(rule)
+                for rule in rules
+            ),
+            rules,
+        )
+
+    def test_handing_sdd_over_adds_no_delegation_target(self):
+        """Handing over is naming the work and pointing at the orchestrator, not
+        delegating to it: the voice still delegates to `pegasus-general` only."""
+        self.assertEqual(self.agent.may_delegate_to, ("pegasus-general",))
+
+    def test_the_ladder_is_pointed_at_not_restated(self):
+        self.assertNotIn(APPLICABILITY_NEEDLE, self.text)
+        for fact in declared_facts(flow_text()):
+            with self.subTest(fact=fact):
+                self.assertNotIn(fact, self.text)
 
 
 class EngramConventionTest(unittest.TestCase):

balerdis/darq

tests/test_orchestrator_routing.py
@@ -39,6 +39,7 @@ SHARED = SKILLS / "_shared"
 
 ORCHESTRATOR = AGENTS / "darq-orchestrator.md"
 APPLICABILITY = SHARED / "flow-applicability.md"
+ARQUITECTO_DARQ = AGENTS / "arquitecto-darq.md"
 CRITERION = SHARED / "sub-delegation-criterion.md"
 
 #: Gate 1's own wording. The orchestrator must point at it, never restate it.
@@ -81,12 +82,53 @@ FTD_NOT_SDD_STEMS = ("trade-off", "session", "record", "outliv", "surviv")
 #: `WordCeilingIsReformatProofTest` below, which proves this on a rewrapped
 #: copy rather than asserting it).
 #:
-#: 1327 words today. The ceiling carries the same proportional headroom the
-#: retired line ceiling did (104 / 97 ~= 1.072x), rounded down: 1327 * 1.07 ~=
-#: 1420. That is room for a short new sentence, not a second section -- a
-#: change that needs more than that earns a deliberate ceiling bump, not a
-#: reflow.
+#: 1327 words when the ceiling was set; 1398 after v7 rewrote the routing
+#: section text for text, which leaves the margin slice (c) needs. The
+#: ceiling carries the same proportional headroom the retired line ceiling
+#: did (104 / 97 ~= 1.072x), rounded down: 1327 * 1.07 ~= 1420. That is
+#: room for a short new sentence, not a second section -- a change that
+#: needs more than that earns a deliberate ceiling bump, not a reflow.
 ORCHESTRATOR_WORD_CEILING = 1420
+
+#: What every ceiling below measures: the WHOLE file, as `wc -w` and
+#: `len(text.split())` count it -- front matter included -- exactly as
+#: `ORCHESTRATOR_WORD_CEILING` above always has, even where prose calls the
+#: file "the body". Subtracting the front matter re-derives a number these
+#: tests never check. Each ceiling is the count measured when it was set plus
+#: only the words already reserved for additions the v7 plan names: no
+#: round-number slack, so drift beyond those additions is caught.
+#:
+#: In this fork every count below was re-measured on its own tree after the
+#: transport, not copied from upstream, and each one came out the same: these
+#: files are upstream's word for word apart from the agent names and the
+#: persona's title, which have the same word count on both sides.
+#:
+#: `flow-applicability.md`: 1026 words measured after v7 a3 (it has no front
+#: matter, so here whole file and prose agree). Reserved: 35 words for b5's
+#: pointer to `_shared/ftd-procedure.md` at the exit of the FTD route clause --
+#: one sentence naming the procedure (record, evidence, close), that it is read
+#: only on that route, and its fail-open, estimated from a draft of that
+#: sentence. 1026 + 35 = 1061. Anything past that pointer earns a deliberate
+#: bump, never a reflow.
+FLOW_APPLICABILITY_WORD_CEILING = 1061
+
+#: `arquitecto-darq.md`: 715 words measured after v7 a5 -- 33 of them front
+#: matter, its two `---` delimiters included. Reserved, estimated from a draft
+#: of each sentence: 43 words for c3 -- about 30 for the readiness wording
+#: beside "Close the loop you open" (outside SDD the readiness call is this
+#: voice's own, made from what it observed, and signing what it wrote is said
+#: as such) and about 13 for the rule that on FTD it keeps the record itself,
+#: reached through `flow-applicability.md`; and 25 words for d5 -- naming
+#: `_shared/implementation-craft.md` and resolving Strict TDD Mode once in its
+#: own session, with that pointer's fail-open. 715 + 43 + 25 = 783.
+ARQUITECTO_DARQ_WORD_CEILING = 783
+
+#: The bodies a word ceiling guards, each with the ceiling it answers to.
+CEILINGED_BODIES = (
+    (ORCHESTRATOR, ORCHESTRATOR_WORD_CEILING),
+    (APPLICABILITY, FLOW_APPLICABILITY_WORD_CEILING),
+    (ARQUITECTO_DARQ, ARQUITECTO_DARQ_WORD_CEILING),
+)
 
 
 def rewrap_preserving_words(text: str) -> str:
@@ -320,34 +362,53 @@ class OrchestratorStaysSmallTest(unittest.TestCase):
         self.assertLessEqual(words, ORCHESTRATOR_WORD_CEILING, "the orchestrator body grew")
 
 
+class FlowApplicabilityStaysSmallTest(unittest.TestCase):
+    """The ladder is read whenever a route is not obvious; it must not grow into
+    the manual of every flow. How FTD runs belongs to its own procedure."""
+
+    def test_the_ladder_did_not_grow_in_hiding(self):
+        words = len(APPLICABILITY.read_text(encoding="utf-8").split())
+        self.assertLessEqual(words, FLOW_APPLICABILITY_WORD_CEILING, "flow-applicability.md grew")
+
+
+class ArquitectoDarqStaysSmallTest(unittest.TestCase):
+    """An always-on body: every word is paid on every turn of the voice."""
+
+    def test_the_teaching_voice_did_not_grow(self):
+        words = len(ARQUITECTO_DARQ.read_text(encoding="utf-8").split())
+        self.assertLessEqual(words, ARQUITECTO_DARQ_WORD_CEILING, "arquitecto-darq.md grew")
+
+
 class WordCeilingIsReformatProofTest(unittest.TestCase):
     """The decisive guard: the measure must not move when only line breaks do.
 
-    Rewraps the real orchestrator body in memory -- the repository file is
-    read and never written -- and asserts the word count this suite relies on is
+    Rewraps each ceilinged body in memory -- the repository files are read and
+    never written -- and asserts the word count this suite relies on is
     unchanged while the line count is, proving the incident in the comment
     above cannot recur under the new measure the way it did under the old one.
     """
 
     def test_rewrapping_moves_lines_but_not_words(self):
-        original = ORCHESTRATOR.read_text(encoding="utf-8")
-        rewrapped = rewrap_preserving_words(original)
+        for body, _ in CEILINGED_BODIES:
+            with self.subTest(body=body.name):
+                original = body.read_text(encoding="utf-8")
+                rewrapped = rewrap_preserving_words(original)
 
-        original_words = len(original.split())
-        rewrapped_words = len(rewrapped.split())
-        original_lines = len(original.splitlines())
-        rewrapped_lines = len(rewrapped.splitlines())
+                original_words = len(original.split())
+                rewrapped_words = len(rewrapped.split())
+                original_lines = len(original.splitlines())
+                rewrapped_lines = len(rewrapped.splitlines())
 
-        self.assertEqual(
-            original_words,
-            rewrapped_words,
-            "the rewrap changed the word count -- it is not a pure reflow",
-        )
-        self.assertNotEqual(
-            original_lines,
-            rewrapped_lines,
-            "the rewrap did not actually change the line layout -- the test proves nothing",
-        )
+                self.assertEqual(
+                    original_words,
+                    rewrapped_words,
+                    "the rewrap changed the word count -- it is not a pure reflow",
+                )
+                self.assertNotEqual(
+                    original_lines,
+                    rewrapped_lines,
+                    "the rewrap did not actually change the line layout -- the test proves nothing",
+                )
 
 
 if __name__ == "__main__":

tests/test_persona_split.py
@@ -28,6 +28,8 @@ import unittest
 from pathlib import Path
 
 from darq.core import content as content_module
+from test_flow_routing import declared_facts, flow_text
+from test_orchestrator_routing import APPLICABILITY_NEEDLE
 
 ROOT = Path(__file__).resolve().parents[1] / "src" / "darq" / "content"
 BASELINE = ROOT / "system-prompt" / "AGENTS.md"
@@ -297,6 +299,67 @@ class PersonaTest(SharedContentRules, unittest.TestCase):
             if not declares(found, level, prefix)
         ]
         self.assertEqual(missing, [], f"the persona lost sections; it has {found}")
+
+
+class ArquitectoDarqRoutesTest(unittest.TestCase):
+    """The teaching voice takes every route but SDD, and reaches the ladder lazily.
+
+    `_shared/flow-applicability.md` owns the four routes; this voice only needs
+    to know where that file is, what to do when it cannot be read, and that
+    work which turns out to need SDD is named and handed to the orchestrator
+    rather than run here. Anything more would be the ladder restated in an
+    always-on body. Like the rest of this module it pins structure -- a path,
+    the house fail-open phrase, two names in one rule -- never a sentence.
+    """
+
+    POINTER = "{{skills_root}}/_shared/flow-applicability.md"
+
+    @classmethod
+    def setUpClass(cls):
+        cls.text = PERSONA.read_text(encoding="utf-8")
+        rules = cls.text.split("\n## Rules\n", 1)[1].split("\n## ", 1)[0]
+        cls.bullets = [" ".join(line[2:].split()) for line in rules.split("\n") if line.startswith("- ")]
+        cls.agent = next(a for a in content_module.load().agents if a.name == "arquitecto-darq")
+
+    def bullets_naming(self, *needles: str) -> list[str]:
+        return [bullet for bullet in self.bullets if all(needle in bullet for needle in needles)]
+
+    def test_a_rule_points_at_the_route_ladder(self):
+        self.assertTrue(self.bullets_naming(self.POINTER), self.bullets)
+
+    def test_the_pointer_fails_open(self):
+        """In the same rule, so the fallback belongs to this reference and not to
+        the delegation-capabilities one that already says it."""
+        self.assertTrue(self.bullets_naming(self.POINTER, "missing or unreadable"), self.bullets)
+
+    def test_work_that_needs_sdd_is_handed_to_the_orchestrator_not_run_here(self):
+        rules = self.bullets_naming("SDD", "`darq-orchestrator`")
+        self.assertTrue(rules, self.bullets)
+        # The negation has to govern "run ... yourself" and the hand-over has to
+        # stand un-negated: a bullet that merely has "never" somewhere before
+        # "run" also passes when it says the opposite ("never hand it over --
+        # run it yourself"), which is the one reading this rule forbids.
+        handed_over = re.compile(r"\bhand(?:s|ed)?\s+(?:it\s+)?over\s+to\s+`darq-orchestrator`")
+        never_run_here = re.compile(r"\b(?:never|not)\s+run\s+(?:it\s+)?yourself\b")
+        negated_hand_over = re.compile(r"\b(?:never|not|no)\b[^.]*\bhand")
+        self.assertTrue(
+            any(
+                handed_over.search(rule) and never_run_here.search(rule) and not negated_hand_over.search(rule)
+                for rule in rules
+            ),
+            rules,
+        )
+
+    def test_handing_sdd_over_adds_no_delegation_target(self):
+        """Handing over is naming the work and pointing at the orchestrator, not
+        delegating to it: the voice still delegates to `darq-general` only."""
+        self.assertEqual(self.agent.may_delegate_to, ("darq-general",))
+
+    def test_the_ladder_is_pointed_at_not_restated(self):
+        self.assertNotIn(APPLICABILITY_NEEDLE, self.text)
+        for fact in declared_facts(flow_text()):
+            with self.subTest(fact=fact):
+                self.assertNotIn(fact, self.text)
 
 
 class EngramConventionTest(unittest.TestCase):